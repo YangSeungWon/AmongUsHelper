cdccd3c3441121015b43b23ddb72e393255f18ce
whysw <ysw1109@postech.ac.kr>
2020-10-01 02:16:53 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Among Us Helper
+Discord bot for Among Us.
+
+## Getting Started
+```
+git clone https://github.com/YangSeungWon/AmongUsHelper.git
+cd AmongUsHelper
+npm install
+```
+Then you can start AmongUsHelper using `node index.js`(Don't forget to configure!).
+
+## Configuration
+1. Follow [Discord Bot Token Guide](https://discordjs.guide/preparations/setting-up-a-bot-application.html#creating-your-bot).
+2. Then copy your bot's TOKEN, and paste it to `config.json`(You can see `HERE!!!` in that file).
+3. Follow [Discord Bot Add Guide](https://discordjs.guide/preparations/adding-your-bot-to-servers.html#bot-invite-links).
+    **You have to add `Send Messages` in TEXT PERMISSIONS, and `Mute Members` in VOICE PERMISSIONS.**
+
+## Commands
+* **!help**
+Print available commands.
+* **!init**
+The voice channel which message sender is joining becomes the target of this bot.
+* **!config N**
+Set conversation time(which includes voting time) to N seconds. default is 135sec.
+* **!status**
+Return meeting status and current setting for conversation time.
+* **!start**
+Start game. SHHHHHHH!
+* **!meet**
+*Please type this when the meeting starts.* It unmutes every alive person. Automatically mutes all after the conversation ends.
+* **!dead**
+*Please type this when you are dead and the next meeting starts.*
+* **!end**
+*Please type this when the game ends.* It unmutes every person.
+
+## Common Usage
+After installation and configuration, including `node index.js`,
+1. Join a voice channel, and type `!init` in any channel.
+2. Using `!config N`, set conversation time as same as your game setting.
+3. When you start game, type `!start`.
+4. When you are killed by imposter, type `!dead`. But after next meeting starts!(because you should not inform others that you're dead.)
+5. When you meeting starts, type `!meet`.
+6. When the game ends, type `!end`.
---
+++ index.js
@@ -0,0 +1,83 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require("./config.json");
+
+
+let voiceChannel;
+let isMeeting = false;
+let time = 135;
+let dead_members = Array();
+
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+client.on('message', msg => {
+    let content = msg.content;
+
+    if (msg.content[0] === '!') {
+        content = content.slice(1);
+        if (content === "help") {
+            msg.reply("\n!meet : 회의 시작(음소거 해제)\n!status : 봇의 현재 상태를 반환합니다.\n!config N : 회의 시간을 N초로 설정합니다.\n!init : 보이스 채널 등록\n!dead : 죽었으면\n!start : 게임 시작\n!end : 게임 끝");
+        } else if (content === "meet") {
+            if (isMeeting) {
+                msg.channel.send("\n이미 회의 중입니다.");
+            } else {
+                unMuteAll().then(console.log);
+                isMeeting = true;
+                let timer = setTimeout(() => {
+                    isMeeting = false;
+                    muteAll().then(console.log);
+                    msg.channel.send("\n회의가 끝났습니다. (모두 음소거)")
+                }, time*1000);
+                msg.channel.send("\n회의를 시작했습니다.(음소거 해제) "+time+"초 후에 종료됩니다.");
+            }
+        } else if (content === "start") {
+            msg.channel.send("\n게임 시작");
+            muteAll().then(console.log);
+        } else if (content === "end") {
+            dead_members.length = 0;
+            msg.channel.send("\n게임 끝");
+            unMuteAll().then(console.log);
+        } else if (content === "status") {
+            if (isMeeting) {
+                msg.reply("\n회의 중입니다.");
+            } else {
+                msg.reply("\n회의 중이 아닙니다.");
+            }
+            msg.reply("\n회의는 "+time+"초 동안 진행됩니다.");
+        } else if (content.startsWith("config ")) {
+            time = parseInt(content.slice(7))
+            msg.reply("\n회의 시간을 "+time+"초로 조정했습니다.");
+        } else if (content === "init") {
+            voiceChannel = msg.member.voice.channel;
+            msg.reply("\n당신의 보이스 채널이 어몽어스 채널로 설정되었습니다.");
+        } else if (content === "dead") {
+            dead_members.push(msg.author.id);
+            msg.member.voice.setMute(true);
+            msg.reply("\nZzz....");
+        }
+    }
+});
+
+client.login(config.token);
+
+async function unMuteAll() {
+    for (let member of voiceChannel.members) {
+        if (!dead_members.includes(member[0])){
+            await member[1].voice.setMute(false);
+        }
+    }
+    return 1;
+}
+
+async function muteAll() {
+    for (let member of voiceChannel.members) {
+        if (!dead_members.includes(member[0])){
+            await member[1].voice.setMute(true);
+        }
+    }
+    return 1;
+}
