c7c37efea20b09217d56dfd5374a481420c16aeb
whysw <ysw1109@postech.ac.kr>
2020-10-06 16:39:20 +0900

    reformed using Promise, acting per voiceChannel.

--- index.js
+++ index.js
@@ -2,12 +2,103 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require("./config.json");
 
+let db = new Array();
 
-let voiceChannel;
-let isGaming = false;
-let isMeeting = false;
-let time = 135;
-let dead_members = Array();
+class Channel {
+    constructor(data){
+        this.channel = data
+        this.channelId = data.id;
+        this.isGaming = false;
+        this.isMeeting = false;
+        this.time = 135;
+        this.dead_members = Array();
+        this.timer;
+    }
+
+	unMuteAll() {
+		for (let member of this.channel.members) {
+			if (!this.dead_members.includes(member[0])){
+				member[1].voice.setMute(false);
+			}   
+		}   
+	}
+
+	muteAll() {
+		for (let member of this.channel.members) {
+			if (!this.dead_members.includes(member[0])){
+				member[1].voice.setMute(true);
+			}   
+		}   
+	}
+    
+    meet(msgChannel){
+        return new Promise((resolve, reject) => {
+            if (!this.isGaming)
+                return reject("\nThe game has not started yet.");
+            if (this.isMeeting)
+                return reject("\nAlready in meeting.");
+
+            this.isMeeting = true;
+            this.unMuteAll();
+            this.timer = setTimeout(() => {
+                this.endMeeting(msgChannel);
+            }, this.time*1000);
+            return resolve("\nStarts the meeting. It ends after "+this.time+" seconds."
+                    +"\nhttps://tenor.com/view/among-us-digibyte-dgb-meme-button-gif-18569623");
+        });
+    }
+
+    endMeeting(msgChannel){
+        this.isMeeting = false;
+        this.muteAll();
+        msgChannel.send("\nThe meeting has ended."
+            +"\nhttps://tenor.com/view/among-us-gif-18540350");
+    }
+
+    start(){
+        return new Promise((resolve, reject) => {
+            if (this.isGaming)
+                return reject("\nThe game has already started.");
+
+            this.isGaming = true;
+            this.dead_members.length = 0;
+            this.muteAll();
+            return resolve("\nThe game begins. SHHHHHHH!"
+                    +"\nhttps://tenor.com/view/among-us-discord-gif-18555996");
+        });
+    }
+
+    end(){
+        return new Promise((resolve, reject) => {
+            if (!this.isGaming)
+                return reject("\nThe game has not started yet.");
+
+            this.isGaming = false;
+            this.dead_members.length = 0;
+            this.unMuteAll();
+            return resolve("\nThe game ended."
+                    +"\nhttps://tenor.com/view/among-us-among-us-victory-just-some-plastic-gif-18613647");
+        });
+    }
+
+    setTime(time){
+        this.time = time;
+    }
+
+    applyDead(personId, voice){
+        return new Promise((resolve, reject) => {
+            if (!this.isGaming)
+                return reject("\nThe game has not started yet.");
+
+            if (this.dead_members.includes(personId))
+                return reject("\nAlready dead :(");
+
+            this.dead_members.push(personId);
+            voice.setMute(true);
+            return resolve("\nhttps://tenor.com/view/among-us-gif-18355819");
+        });
+    }
+}
 
 
 client.on('ready', () => {
@@ -17,88 +108,83 @@ client.on('ready', () => {
 
 client.on('message', msg => {
     let content = msg.content;
+    let voiceChannel = msg.member.voice.channel;
 
-    if (msg.content[0] === '!') {
+    if (msg.content[0] === '!')
+    {
         content = content.slice(1);
-        if (content === "help") {
-            msg.reply("\n!meet : 회의 시작(음소거 해제)\n!status : 봇의 현재 상태를 반환합니다.\n!config N : 회의 시간을 N초로 설정합니다.\n!init : 보이스 채널 등록\n!dead : 죽었으면\n!start : 게임 시작\n!end : 게임 끝");
-        } else if (content === "meet") {
-            if (isMeeting) {
-                msg.channel.send("\n이미 회의 중입니다.");
+
+        if (!voiceChannel){
+            msg.reply("\nPlease join voice channel.");
+            return;
+        }
+        
+        let obj = db.find(ch => ch.channelId === voiceChannel.id);
+        if (!obj){
+            db.push(new Channel(voiceChannel));
+            obj = db.find(ch => ch.channelId === voiceChannel.id);
+        }
+        
+        if (content === "help")
+        {
+            msg.reply("\n!config N : set meeting time to N\n!status : return current status\n!meet : start meeting\n!start : start game\n!end : end game\n!dead : broadcast your death");
+        } 
+        else if (content.startsWith("config "))
+        {
+            let time = parseInt(content.slice(7));
+            obj.setTime(time);
+            msg.reply("\nMeeting after this message will lasts for "+time+" seconds.");
+        }
+        else if (content === "status")
+        {
+            let retMsg = "";
+            if (obj.isGaming) {
+                retMsg += "\nIn Gaming...";
             } else {
-                unMuteAll().then(console.log);
-                isMeeting = true;
-                let timer = setTimeout(() => {
-                    isMeeting = false;
-                    muteAll().then(console.log);
-                    msg.channel.send("\n회의가 끝났습니다. (모두 음소거)");
-                    msg.channel.send("\nhttps://tenor.com/view/among-us-gif-18540350");
-                }, time*1000);
-                msg.channel.send("\n회의를 시작했습니다.(음소거 해제) "+time+"초 후에 종료됩니다.");
-                msg.channel.send("\nhttps://tenor.com/view/among-us-digibyte-dgb-meme-button-gif-18569623");
+                retMsg += "\nNot in Gaming...";
             }
-        } else if (content === "start") {
-            if (isGaming) {
-                msg.reply("\n이미 게임 중입니다.");
+            if (obj.isMeeting) {
+                retMsg += "\nIn Meeting...";
             } else {
-                isGaming = true;
-                dead_members.length = 0;
-                msg.channel.send("\n게임 시작");
-                msg.channel.send("\nhttps://tenor.com/view/among-us-discord-gif-18555996");
-                muteAll().then(console.log);
+                retMsg += "\nNot in Meeting...";
             }
-        } else if (content === "end") {
-            if (!isGaming) {
-                msg.reply("\n게임 중이 아닙니다.");
-            } else {
-                isGaming = false;
-                dead_members.length = 0;
-                msg.channel.send("\n게임 끝");
-                msg.channel.send("\nhttps://tenor.com/view/among-us-among-us-victory-just-some-plastic-gif-18613647");
-                unMuteAll().then(console.log);
-            }
-        } else if (content === "status") {
-            if (isGaming) {
-                msg.reply("\n게임 중입니다.");
-            } else {
-                msg.reply("\n게임 중이 아닙니다.");
-            }
-            if (isMeeting) {
-                msg.reply("\n회의 중입니다.");
-            } else {
-                msg.reply("\n회의 중이 아닙니다.");
-            }
-            msg.reply("\n회의는 "+time+"초 동안 진행됩니다.");
-        } else if (content.startsWith("config ")) {
-            time = parseInt(content.slice(7))
-            msg.reply("\n회의 시간을 "+time+"초로 조정했습니다.");
-        } else if (content === "init") {
-            voiceChannel = msg.member.voice.channel;
-            msg.reply("\n당신의 보이스 채널이 어몽어스 채널로 설정되었습니다.");
-        } else if (content === "dead") {
-            dead_members.push(msg.author.id);
-            msg.member.voice.setMute(true);
-            msg.reply("\nhttps://tenor.com/view/among-us-gif-18355819");
+            retMsg += "\nMeeting lasts for "+obj.time+" seconds.";
+            msg.reply(retMsg);
+        }
+        else if (content === "meet") 
+        {
+            obj.meet(msg.channel).then(retMsg => {
+                msg.channel.send(retMsg);
+            }).catch(err => {
+                msg.channel.send(err);
+            });
+        }
+        else if (content === "start")
+        {
+            obj.start().then(retMsg => {
+                msg.channel.send(retMsg);
+            }).catch(err => {
+                msg.channel.send(err);
+            });
+        }
+        else if (content === "end")
+        {
+            obj.end().then(retMsg => {
+                msg.channel.send(retMsg);
+            }).catch(err => {
+                msg.channel.send(err);
+            });
+        }
+        else if (content === "dead")
+        {
+            obj.applyDead(msg.author.id, msg.member.voice).then(retMsg => {
+                msg.channel.send(retMsg);
+            }).catch(err => {
+                msg.channel.send(err);
+            });
         }
     }
 });
 
 client.login(config.token);
 
-async function unMuteAll() {
-    for (let member of voiceChannel.members) {
-        if (!dead_members.includes(member[0])){
-            await member[1].voice.setMute(false);
-        }
-    }
-    return 1;
-}
-
-async function muteAll() {
-    for (let member of voiceChannel.members) {
-        if (!dead_members.includes(member[0])){
-            await member[1].voice.setMute(true);
-        }
-    }
-    return 1;
-}
